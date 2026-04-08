9d348bdbe8e5fe60f3da7bb65d7ca9a278cd809b
CkYes-DBT <23502410026@student.prasetiyamulya.ac.id>
2026-04-08 16:12:26 +0700

    admin_n_urlsApp

--- backend/qrpay/urls.py
+++ backend/qrpay/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from .views import GenerateQRView, ResolveQRView
+from .views import QrisListCreateView, QrisDetailView, QrisScanView
+from .models import Qris
+from .serializers import QrisSerializer
 
 urlpatterns = [
     path('generate/', GenerateQRView.as_view()),
     path('resolve/<int:user_id>/', ResolveQRView.as_view()),
+    path('', QrisListCreateView.as_view()),
+    path('scan/', QrisScanView.as_view()),
+    path('<int:qris_id>/', QrisDetailView.as_view()),
 ]
